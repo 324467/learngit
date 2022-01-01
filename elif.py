59451377daddeefa258b6fe58748d51c9e8130e3
ezlio <1217372635@qq.com>
2022-01-01 15:36:01 +0800

    add space

--- elif.py
+++ elif.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
+
 h = input('请输入你的身高（m）：')
 w = input('请输入你的体重(kg)：')
 h = float(h)
 w = float(w)
 bmi = w/h**2
-#print(bmi)
+
 if bmi >= 32:
     print('您的bmi指数为%.1f，严重肥胖'%(bmi))
 elif bmi >= 28:
